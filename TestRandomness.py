fd9636777651c77cad28c25ebe02d8874cc28c30
lorsem <lorsem@hotmail.it>
2015-02-25 23:00:32 +0100

    Fixes

--- TestRandomness.py
+++ TestRandomness.py
@@ -4,11 +4,18 @@ from __future__ import division
 
 def calcolaStatisticaTest(dati):
     #Dividiamo l'intervallo [0,1] in k segmenti, e contiamo le occorrenze campionarie
-    
+    datip = []
+    for j in range(len(dati)):
+        if float(dati[j]) < 0:
+            continue
+        else:
+            datip.append(float(dati[j]))
+    dati = datip            
     k = 5
     occorrenzeCampionarie = [0]*k
 
     for valore in dati:
+        if float(valore) < 0: continue
         for i in range(k):
             
             if (i/k) <=  float(valore) < ((i+1)/k):
@@ -19,7 +26,7 @@ def calcolaStatisticaTest(dati):
     
     sum = 0
     n = len(dati)
-    for i in range(n):
+    for i in range(k):
         sum += (occorrenzeCampionarie[i]**2)/(n*(1/k))
     sum -= n
 
@@ -28,7 +35,7 @@ def calcolaStatisticaTest(dati):
 
 
 if __name__ == "__main__":
-    with open('./test.txt') as f:
+    with open('./output.txt') as f:
         lines = f.read().splitlines()
     
     calcolaStatisticaTest(lines)
