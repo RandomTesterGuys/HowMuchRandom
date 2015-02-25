0da72b6d997fe850b12f9d5c86accc4899214440
Ascanio Vimercati Sanseverino <Ascanio@MBP-di-Ascanio.station>
2015-02-25 21:58:29 +0100

    Pregressi

--- TestRandomness.py
+++ TestRandomness.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
 
+from __future__ import division
+
+def calcolaStatisticaTest(dati):
+    #Dividiamo l'intervallo [0,1] in k segmenti, e contiamo le occorrenze campionarie
+    
+    k = 5
+    occorrenzeCampionarie = [0]*k
+
+    for valore in dati:
+        print "Valore: " + valore
+
+        for n in range(k):
+            print n/k
+            print (n+1)/k
+            
+            if (valore >= (n/k) and  valore < ((n+1)/k)):
+                occorrenzeCampionarie[n] = occorenzeCampionarie[n] +1
+
+    print dati
+    print occorrenzeCampionarie
 
 if __name__ == "__main__":
     with open('./test.txt') as f:
         lines = f.read().splitlines()
     
-    print lines
-    print "Done!"
+    calcolaStatisticaTest(lines)
+
