05727102ea4225b5d8a55f3bb9f80f9e842661be
lorsem <lorsem@hotmail.it>
2015-02-25 21:32:58 +0100

    Working things

--- hardware_random.c
+++ hardware_random.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main() {
     int i = 9;
-    int var = 999;
+    int varloc = 678;
+    float var = 999;
     FILE* output = fopen("output.txt", "w");
     for (i=0; i<10000; i++)
     {
-        fprintf(output, "%d\n", var);
+        fprintf(output, "%f\n", var/9223372036854775808.0);
     }
 }
